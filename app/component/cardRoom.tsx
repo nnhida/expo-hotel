2b4e299ff91efd4dea736951322be2179f7eae75
unknown <pertamaham@gmail.com>
2024-12-09 11:46:54 +0700

    revisi css cardRoom

--- app/component/cardRoom.tsx
+++ app/component/cardRoom.tsx
@@ -26,7 +26,7 @@ export default function CardRoom({nama_tipe_kamar,harga,foto,deskripsi, auth}:ca
     <div className='group relative w-max'>
       <Toaster richColors/>
         <div className='w-60 h-96 relative overflow-hidden shadow-xl rounded-3xl bg-slate-400 bg-center bg-cover' style={{backgroundImage: `url("/upload/tipe_kamar/${foto}")`}}>
-            <div className=' absolute -bottom-80 group-hover:bottom-0 p-5 flex flex-col space-y-2 h-max bg-white bg-opacity-50 transition-all'>
+            <div className=' absolute -bottom-80 group-hover:bottom-0 p-5 flex flex-col space-y-2 h-max w-full bg-white bg-opacity-50 transition-all'>
                 <p className='text-2xl font-bold'>{nama_tipe_kamar}</p>
                 <p className='text-lg text-white w-max p-1 rounded-xl bg-green-400 font-bold'>Rp. {harga}</p>
                 <p className='line-clamp-3'>{deskripsi}</p>
